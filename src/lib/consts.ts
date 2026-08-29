b23c18f6b794ec60013eea78d0835fb7f35e7fd8
Zulfaabam <zulfafatahakbar@gmail.com>
2026-08-29 07:42:17 +0700

    feat: add navigation component and SVG icons; define constants for social media, menu, and journeys

---
+++ src/lib/consts.ts
@@ -0,0 +1,264 @@
+export const INTRO =
+  "Software Engineer with 3+ years of experience building production-grade web applications using React, Next.js, and TypeScript. Experienced in developing scalable web app, maintainable UI architectures, integrating REST APIs, and collaborating with cross-functional teams. Now focusing on C# .NET project at Formulatrix.";
+
+export const socmedList = [
+  {
+    id: 1,
+    title: "LinkedIn",
+    link: "https://www.linkedin.com/in/zulfa-fatah-akbar-ahmad/",
+    src: "https://i.ibb.co/wzK5Jnd/linkedin-icon.png",
+    alt: "LinkedIn icon",
+  },
+  {
+    id: 2,
+    title: "GitHub",
+    link: "https://github.com/Zulfaabam",
+    src: "https://i.ibb.co/tmjnwZq/github-icon.png",
+    alt: "Github icon",
+  },
+  {
+    id: 3,
+    title: "Instagram",
+    link: "https://www.instagram.com/zfaabam/",
+    src: "https://i.ibb.co/41fYsJz/instagram-icon.png",
+    alt: "Instagram icon",
+  },
+];
+
+export const menu = [
+  {
+    id: 1,
+    path: "/",
+    title: "Home",
+  },
+  {
+    id: 2,
+    path: "/journey",
+    title: "Journey",
+  },
+  {
+    id: 3,
+    path: "/projects",
+    title: "Projects",
+  },
+  {
+    id: 4,
+    path: "/labs",
+    title: "Labs",
+  },
+  {
+    id: 5,
+    path: "https://abams.hashnode.dev/",
+    title: "Blog",
+    isOutsideLink: true,
+  },
+];
+
+export const techStack = [
+  { name: "React", id: 1 },
+  { name: "Next.js", id: 2 },
+  { name: "TailwindCSS", id: 3 },
+  { name: "TypeScript", id: 8 },
+];
+
+export const MINI_JOURNEYS = [
+  {
+    id: 1,
+    job: "Software Engineer Intern @ Formulatrix",
+    place: "Salatiga, Indonesia",
+    year: "Now",
+  },
+  {
+    id: 2,
+    job: "Frontend Dev @ Eksad",
+    place: "Jakarta, Indonesia",
+    year: "2025",
+  },
+  {
+    id: 3,
+    job: "Frontend Dev @ Bukit Vista",
+    place: "Bali, Indonesia",
+    year: "2022 - 2024",
+  },
+];
+
+export const galleryImages = [
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286145/20240319_214614_d1he1g_pnn5wp.jpg",
+    title: "Gundam Wing Zero EW",
+    category: "Toys",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286072/20250125_200015_vmx7yn_s8zi41.webp",
+    title: "Green Setup",
+    category: "Workspace",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286069/20241119_151254_px8v9g_ted3ds.webp",
+    title: "Crocodile On the Beach",
+    category: "Holiday",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286070/20250125_201639_ssnm1c_txver7.webp",
+    title: "Blue Setup",
+    category: "Workspace",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286063/20250405_140307_zpwvb2_qxtn3o.webp",
+    title: "Parking Lot View",
+    category: "Holiday",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286061/20241130_100222_d2orgz_a601xn.webp",
+    title: "Me in Semirang",
+    category: "Holiday",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286060/20241130_104724_bazq4q_mzg2na.webp",
+    title: "Random Rocks Formation",
+    category: "Holiday",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286048/20240226_214247_eitgzc_wrzpq6.webp",
+    title: "Gundam Load Astray",
+    category: "Toys",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286043/20240811_135825_lxartm_tsr9bv.webp",
+    title: "Sea Keyboard",
+    category: "Setup",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770286043/20250702_070554_vqzypf_euscj3.webp",
+    title: "Jogging View",
+    category: "Workout",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770283211/20250712_215450_1_j6axof.jpg",
+    title: "A Happy Cat",
+    category: "Pets",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770283208/20250802_223058_uk5vty.jpg",
+    title: "Gojo In My Setup",
+    category: "Workspace",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770283207/20260203_071828_drkzhg.jpg",
+    title: "Palico Cat",
+    category: "Pets",
+  },
+  {
+    src: "https://res.cloudinary.com/dx34xih1p/image/upload/v1770283213/20251121_143131_ffreri.jpg",
+    title: "Croissant",
+    category: "Foods",
+  },
+];
+
+export const JOURNEY_2025 = {
+  year: "2025",
+  company: "PT Tiga Daya Digital Indonesia",
+  jobTitle: "Frontend Developer",
+  duration: "January 2025 - December 2025",
+  image1:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1764379814/me_in_asl_fj6pef.jpg",
+  image2:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1770965503/vision_customer_page_h029to.webp",
+  image3:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1764378195/Lelang-Elektronik-JBA-Indonesia_gqigjg.png",
+  projects: [
+    "JBA Indonesia’s Electronic Auction platform",
+    "JAVIS - The internal team’s app to manage content for auction",
+    "VISION - The new internal team’s app to manage content for auction",
+  ],
+  tech: [
+    "React",
+    "Next.js",
+    "TypeScript",
+    "Tailwind CSS",
+    "Storybook",
+    "BitBucket",
+    "Jira",
+  ],
+};
+
+export const JOURNEY_2023_2024 = {
+  year: "2023 - 2024",
+  company: "PT Bukit Vista Nusantara",
+  jobTitle: "Frontend Developer",
+  duration: "August 2023 - August 2024",
+  image1:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731677218/DSC08857_l4mkg9.jpg",
+  image2:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731678675/Screenshot_2024-06-29_160042_dq7ivp.png",
+  image3:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731507569/tinker_lzjuyc.png",
+  projects: [
+    "Maintained BIGRR - Bukit Vista's internal app to manage properties, booking calendar, finance, and more",
+    "Built Tinker - a debugging tool for Bukit Vista's AI called Gaia",
+    "Small contribution in BVGO app using Flutter and Bukit Vista's Website using WordPress",
+  ],
+  tech: [
+    "React",
+    "JavaScript",
+    "Tailwind CSS",
+    "Material UI",
+    "Redux",
+    "WordPress",
+    "Flutter",
+    "GitHub",
+    "Trello",
+  ],
+};
+
+export const JOURNEY_2022_2023 = {
+  year: "2022 - 2023",
+  company: "PT Bukit Vista Nusantara",
+  jobTitle: "Part-time Frontend Developer",
+  duration: "July 2022 - August 2023",
+  image1:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731679863/bigrr-bookings_weujrk.png",
+  image2:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731680448/guest-portal-es_ymmjyi.png",
+  image3:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731678851/bvw-event-calendar_a3isgq.png",
+  projects: [
+    "Maintained BIGRR - Bukit Vista's internal app to manage properties, booking calendar, finance, and more",
+    "Refactored almost all of BIGRR's features using modern React best practices: lazy loading, code splitting, Context API, and more",
+  ],
+  tech: [
+    "React",
+    "JavaScript",
+    "Tailwind CSS",
+    "Material UI",
+    "Redux",
+    "GitHub",
+    "Trello",
+  ],
+};
+
+export const JOURNEY_2022 = {
+  year: "2022",
+  company: "PT Bukit Vista Nusantara",
+  jobTitle: "Frontend Developer Intern",
+  duration: "February 2022 - June 2022",
+  image1:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731679451/certif-intern-bv_mf1pyu.png",
+  image2:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731679594/ss_prop_list_new_2_bpnxlk.png",
+  image3:
+    "https://res.cloudinary.com/dx34xih1p/image/upload/v1731678390/Screenshot_2024-08-14_160727_eoxzxa.png",
+  projects: [
+    "Refactored some of BIGRR's features by splitting code logic, create and use reusable components, better Redux state management",
+    "Learning and implementing React best practices",
+  ],
+  tech: [
+    "React",
+    "JavaScript",
+    "Tailwind CSS",
+    "Material UI",
+    "Redux",
+    "GitHub",
+    "Trello",
+  ],
+};
